ee98940bd0a366be4ed3e419ad38b0f39eeeb309
Hoang Huy <huyhoangrt009@gmail.com>
2023-06-07 11:39:18 +0700

    lab1

---
+++ film/src/App.js
@@ -0,0 +1,12 @@
+import'./App.css';
+import Navigation from './Component/navigation';
+import Films from './Component/films';
+ function App(){
+  return(
+    <div className='App'>
+      <Navigation/>
+      <Films/>
+    </div>
+  )
+}
+export default App;
---
+++ film/src/Component/films.js
@@ -0,0 +1,81 @@
+import React, {Component} from 'react'
+export default class Films extends Component{
+    render(){
+        return(
+            <div className='container'>
+        <div className='column'>
+            <div className='card'>
+                <img src='assets/image/380b010d0c000d-315x420 (Small).png'/>
+                <h3>Train of Busan</h3>
+                <p className='title'>Zombie</p>
+                <p><button>Detail</button></p>
+            </div>
+        </div>
+        <div className='column'>
+            <div className='card'>
+                <img src='512323-0.jpg'/>
+                <h3>No Game No Life</h3>
+                <p className='title'>anime</p>
+                <p><button>Detail</button></p>
+            </div>
+        </div>
+        <div className='column'>
+            <div className='card'>
+                <img src='nhung-bo-phim-chieu-rap-2021-hay-nhat-hinh-7.jpg'/>
+                <h3>The Counjuring</h3>
+                <p className='title'>horrified</p>
+                <p><button>Detail</button></p>
+            </div>
+        </div>
+        <div className='column'>
+            <div className='card'>
+                <img src='phim-chieu-rap-dang-mong-cho-trong-thang-11-nam-2016-23216.jpg'/>
+                <h3>The Troll</h3>
+                <p className='title'>Cartoon</p>
+                <p><button>Detail</button></p>
+            </div>
+        </div>
+        <div className='column'>
+            <div className='card'>
+                <img src='phim-viet-nam-chieu-rap-9.jpg'/>
+                <h3>Trai tim quai vat</h3>
+                <p className='title'>horrified</p>
+                <p><button>Detail</button></p>
+            </div>
+        </div>
+        <div className='column'>
+            <div className='card'>
+                <img src='th (1).jfif'/>
+                <h3>Doraemon movie</h3>
+                <p className='title'>anime</p>
+                <p><button>Detail</button></p>
+            </div>
+        </div>
+        <div className='column'>
+            <div className='card'>
+                <img src='th (2).jfif'/>
+                <h3>Zootopia</h3>
+                <p className='title'>Cartoon</p>
+                <p><button>Detail</button></p>
+            </div>
+        </div>
+        <div className='column'>
+            <div className='card'>
+                <img src='th.jfif'/>
+                <h3>Hai Phuong</h3>
+                <p className='title'>Action</p>
+                <p><button>Detail</button></p>
+            </div>
+        </div>
+        <div className='column'>
+            <div className='card'>
+                <img src='top-10-bo-phim-chieu-rap-dang-xem-nhat-nam-2020-maxresdefault-13.jpg'/>
+                <h3>Nắng 3</h3>
+                <p className='title'>humorous</p>
+                <p><button>Detail</button></p>
+            </div>
+        </div>
+    </div>
+        )
+    }
+}
